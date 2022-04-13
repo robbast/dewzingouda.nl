38de2bdd68384d6e32f045e22ee320992a9f00c9
Rob Bast <rob.bast@gmail.com>
2022-04-13 14:08:52 +0200

    update audo filenames, add all questions

--- src/app.tsx
+++ src/app.tsx
@@ -6,40 +6,217 @@ const data: QuestionProps[] = [
   {
     image: 'https://picsum.photos/400/300',
     audio: '/audio/arti-legi.m4a',
-    title: 'Gouwekerk',
+    title: 'Arti Legi',
     question: 'Hoeveel meter hoog is de Gouwekerk?',
-    answer: 80
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
   },
   {
     image: 'https://picsum.photos/400/300',
-    audio: '/audio/Arti Legi.m4a',
-    title: 'Sint-Janskerk',
+    audio: '/audio/carillon-gouda.m4a',
+    title: 'Carillon',
     question: 'Hoeveel gebrandschilderde glazen telt de Sint-Janskerk?',
-    answer: 72
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
   },
   {
     image: 'https://picsum.photos/400/300',
-    audio: '/audio/Arti Legi.m4a',
-    title: 'Stadsrechten',
+    audio: '/audio/catherina-gasthuis.m4a',
+    title: 'Catherina Gasthuis',
     question: 'Hoe lang is Gouda al een stad?',
-    answer: 750
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
   },
   {
     image: 'https://picsum.photos/400/300',
-    audio: '/audio/Arti Legi.m4a',
-    title: 'Beroemd',
-    question: 'Waar kent iedereen Gouda van?',
-    answer: 'kaas'
+    audio: '/audio/de-waag-gouda.m4a',
+    title: 'De Waag',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
   },
   {
     image: 'https://picsum.photos/200/300',
-    audio: '/audio/Arti Legi.m4a',
-    title: 'Multiple-choice',
+    audio: '/audio/donkere-sluis.m4a',
+    title: 'Donkere Sluis',
     question: 'Wat moesten Goudse bewoners weg stoppen?',
-    answer: [
-      'pijpen',
-      'kaarsen',
-      'aanstekers'
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/dubbele-buurt.m4a',
+    title: 'Dubbele Buurt',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/fietsen.m4a',
+    title: 'Fietsen',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/joods-bejaardenhuis.m4a',
+    title: 'Joods Bejaardenhuis (De Haven)',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/kees-faessens-rolwagensteeg.m4a',
+    title: 'Kees Faessens Rolwagensteeg',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/kerktoren.m4a',
+    title: 'Kerktoren',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/korte-tiendeweg.m4a',
+    title: 'Korte Tiendeweg',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/oosthaven.m4a',
+    title: 'Oosthaven',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/sint-janskerk.m4a',
+    title: 'Sint-Janskerk',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/st.aloysius-school.m4a',
+    title: 'St. Aloysius School',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/stroopwafelmuur.m4a',
+    title: 'Stroopwafelmuur',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/van-der-vormkapel.m4a',
+    title: 'Van der Vormkapel',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/voorzijde-stadhuis.m4a',
+    title: 'Stadhuis',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: 'https://picsum.photos/200/300',
+    audio: '/audio/willem-vroesenhuis.m4a',
+    title: 'Willem Vroesenhuis',
+    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
     ]
   }
 ]
--- src/question.tsx
+++ src/question.tsx
@@ -5,7 +5,8 @@ type QuestionProps = {
   audio: string,
   title: string,
   question: string,
-  answer: number | string | (number | string)[]
+  answerIndex: number,
+  answers: (number | string)[]
 }
 
 export type { QuestionProps }
@@ -17,27 +18,14 @@ export default class Question extends React.PureComponent<QuestionProps & ClassA
   }
 
   render() {
-    const { answer } = this.props
-
-    let input: JSX.Element | JSX.Element[] | null = null
-
-    if (typeof answer === 'number') {
-      input = <input type="number" placeholder="Antwoord.." name="answer" className="form-control" />
-    }
-
-    if (typeof answer === 'string') {
-      input = <input type="text" placeholder="Antwoord.." name="answer" className="form-control" />
-    }
-
-    if (typeof answer === 'object' && true === Array.isArray(this.props.answer)) {
-      input = answer.map((value: string | number, index: number) => {
+    const { answerIndex, answers } = this.props,
+      input: JSX.Element | JSX.Element[] = answers.map((value: string | number, index: number) => {
         const id = `answer-${index}`
         return <div className="form-check text-align-left">
           <input className="form-check-input" type="radio" id={id} name="answer" value="value" />
           <label className="form-check-label" htmlFor={id}>{value}</label>
         </div>
       })
-    }
 
     return <div style={{ height: '300px' }} className="text-center">
       <img src={this.props.image} style={{ margin: '0 auto 1.5rem', maxWidth: '100%', maxHeight: '100%', borderRadius: '.5rem', objectFit: 'contain' }} />
