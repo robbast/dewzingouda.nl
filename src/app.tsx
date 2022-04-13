79b9b5554005c0fd8cd0c42b9253e357b19ca919
Rob Bast <rob.bast@gmail.com>
2022-04-13 15:01:31 +0200

    rename audio and images according to context, add questions

--- src/app.tsx
+++ src/app.tsx
@@ -4,10 +4,10 @@ import Question, { QuestionProps } from './question'
 
 const data: QuestionProps[] = [
   {
-    image: 'https://picsum.photos/400/300',
+    image: '/images/arti-legi.jpg',
     audio: '/audio/arti-legi.m4a',
     title: 'Arti Legi',
-    question: 'Hoeveel meter hoog is de Gouwekerk?',
+    question: 'Waar staat het woord "legi" voor?',
     answerIndex: 0,
     answers: [
       'a',
@@ -16,10 +16,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/400/300',
-    audio: '/audio/carillon-gouda.m4a',
+    image: '/images/carillon.jpg',
+    audio: '/audio/carillon.m4a',
     title: 'Carillon',
-    question: 'Hoeveel gebrandschilderde glazen telt de Sint-Janskerk?',
+    question: 'Waar komt het melodietje van het carillon vandaan?',
     answerIndex: 0,
     answers: [
       'a',
@@ -28,10 +28,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/400/300',
-    audio: '/audio/catherina-gasthuis.m4a',
-    title: 'Catherina Gasthuis',
-    question: 'Hoe lang is Gouda al een stad?',
+    image: '/images/catharina-gasthuis.jpg',
+    audio: '/audio/catharina-gasthuis.m4a',
+    title: 'Catharina Gasthuis',
+    question: 'Wanneer hield het Catharina Gasthuis op met bestaan als ziekenhuis?',
     answerIndex: 0,
     answers: [
       'a',
@@ -40,10 +40,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/400/300',
-    audio: '/audio/de-waag-gouda.m4a',
+    image: '/images/de-waag.jpg',
+    audio: '/audio/de-waag.m4a',
     title: 'De Waag',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Vanaf wanneer bestaat de Goudse Kaasmarkt?',
     answerIndex: 0,
     answers: [
       'a',
@@ -52,10 +52,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/donkere-sluis.jpg',
     audio: '/audio/donkere-sluis.m4a',
     title: 'Donkere Sluis',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Wanneer werd de Donkere Sluis gebouwd?',
     answerIndex: 0,
     answers: [
       'a',
@@ -64,10 +64,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/dubbele-buurt.jpg',
     audio: '/audio/dubbele-buurt.m4a',
     title: 'Dubbele Buurt',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Hoeveel Dubbele Buurt straten waren er vroeger in Gouda?',
     answerIndex: 0,
     answers: [
       'a',
@@ -76,10 +76,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/fietsen.jpg',
     audio: '/audio/fietsen.m4a',
     title: 'Fietsen',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Hoeveel fietsen waren er een paar weken geleden in deze steeg?',
     answerIndex: 0,
     answers: [
       'a',
@@ -88,10 +88,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/joods-bejaardenhuis.jpg',
     audio: '/audio/joods-bejaardenhuis.m4a',
     title: 'Joods Bejaardenhuis (De Haven)',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Hoeveel tegeltjes voor vermoorde Joodse mensen tel je?',
     answerIndex: 0,
     answers: [
       'a',
@@ -100,10 +100,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/kees-faessens-rolwagensteeg.jpg',
     audio: '/audio/kees-faessens-rolwagensteeg.m4a',
     title: 'Kees Faessens Rolwagensteeg',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Uit welk jaar kwam de jaarrekening waarin staat wie Kees Faessens was?',
     answerIndex: 0,
     answers: [
       'a',
@@ -112,22 +112,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
-    audio: '/audio/kerktoren.m4a',
-    title: 'Kerktoren',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
-    answerIndex: 0,
-    answers: [
-      'a',
-      'b',
-      'c'
-    ]
-  },
-  {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/korte-tiendeweg.jpg',
     audio: '/audio/korte-tiendeweg.m4a',
     title: 'Korte Tiendeweg',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Hoeveel tiendwegen zijn er nog in Nederland?',
     answerIndex: 0,
     answers: [
       'a',
@@ -136,10 +124,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/oosthaven.jpg',
     audio: '/audio/oosthaven.m4a',
     title: 'Oosthaven',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Hoeveel schepen voeren er vroeger jaarlijks door de grachten?',
     answerIndex: 0,
     answers: [
       'a',
@@ -151,7 +139,7 @@ const data: QuestionProps[] = [
     image: 'https://picsum.photos/200/300',
     audio: '/audio/sint-janskerk.m4a',
     title: 'Sint-Janskerk',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Hoeveel gebrandschilderde glas-in-lood glazen telt de Sint-Janskerk?',
     answerIndex: 0,
     answers: [
       'a',
@@ -160,10 +148,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/st.aloysius-school.jpg',
     audio: '/audio/st.aloysius-school.m4a',
     title: 'St. Aloysius School',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Wanneer is de St. Aloysius School ontworpen?',
     answerIndex: 0,
     answers: [
       'a',
@@ -172,10 +160,22 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/stadhuis.jpg',
+    audio: '/audio/stadhuis.m4a',
+    title: 'Stadhuis',
+    question: 'Wat is de bijnaam van Maria van Bourgondië?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: '/images/stroopwafelmuur.jpg',
     audio: '/audio/stroopwafelmuur.m4a',
     title: 'Stroopwafelmuur',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Wanneer opende Gerard Kamphuisen zijn bakkerij met echte stroopwafels?',
     answerIndex: 0,
     answers: [
       'a',
@@ -184,10 +184,22 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/toren-sint-janskerk.jpg',
+    audio: '/audio/toren-sint-janskerk.m4a',
+    title: 'Kerktoren',
+    question: 'Hoeveel jaar heeft Gouda stadsrechten?',
+    answerIndex: 0,
+    answers: [
+      'a',
+      'b',
+      'c'
+    ]
+  },
+  {
+    image: '/images/van-der-vormkapel.jpg',
     audio: '/audio/van-der-vormkapel.m4a',
     title: 'Van der Vormkapel',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Voor welk gebouw waren de gebrandschilderde glas-in-lood glazen van deze kapel eigenlijk bedoelt?',
     answerIndex: 0,
     answers: [
       'a',
@@ -196,22 +208,10 @@ const data: QuestionProps[] = [
     ]
   },
   {
-    image: 'https://picsum.photos/200/300',
-    audio: '/audio/voorzijde-stadhuis.m4a',
-    title: 'Stadhuis',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
-    answerIndex: 0,
-    answers: [
-      'a',
-      'b',
-      'c'
-    ]
-  },
-  {
-    image: 'https://picsum.photos/200/300',
+    image: '/images/willem-vroesenhuis.jpg',
     audio: '/audio/willem-vroesenhuis.m4a',
     title: 'Willem Vroesenhuis',
-    question: 'Wat moesten Goudse bewoners weg stoppen?',
+    question: 'Wat moesten bejaarde Goudenaars achterlaten in de kluisjes?',
     answerIndex: 0,
     answers: [
       'a',
--- src/question.tsx
+++ src/question.tsx
@@ -19,26 +19,27 @@ export default class Question extends React.PureComponent<QuestionProps & ClassA
 
   render() {
     const { answerIndex, answers } = this.props,
-      input: JSX.Element | JSX.Element[] = answers.map((value: string | number, index: number) => {
+      buttons: JSX.Element | JSX.Element[] = answers.map((value: string | number, index: number) => {
         const id = `answer-${index}`
-        return <div className="form-check text-align-left">
-          <input className="form-check-input" type="radio" id={id} name="answer" value="value" />
-          <label className="form-check-label" htmlFor={id}>{value}</label>
-        </div>
+        return <button type="button" className="btn btn-primary" style={{ color: '#eb6334', backgroundColor: '#fdce43', borderColor: '#fdce43' }}>{value}</button>
       })
 
-    return <div style={{ height: '300px' }} className="text-center">
-      <img src={this.props.image} style={{ margin: '0 auto 1.5rem', maxWidth: '100%', maxHeight: '100%', borderRadius: '.5rem', objectFit: 'contain' }} />
-      <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+    return <>
+      <div className="row p-0 g-0 mb-4" style={{ height: '480px' }}>
+        <img src={this.props.image} style={{ maxWidth: '100%', maxHeight: '100%', borderRadius: '1rem', objectFit: 'cover' }} />
+      </div>
+      <div className="row p-0 g-0 d-flex">
         <audio controls preload="auto" src={this.props.audio} style={{ margin: 'auto' }}>
           Your browser does not support the audio element.
         </audio>
       </div>
-      <div className="text-start" style={{ color: '#fff' }}>
-        <h1 style={{ color: '#fcb900' }} className="mt-4 mb-2 text-center">{this.props.title}</h1>
+      <div className="row text-center" style={{ color: '#fff' }}>
+        <h1 style={{ color: '#fcb900' }} className="mt-4 mb-2">{this.props.title}</h1>
         <p>{this.props.question}</p>
-        {input}
+        <div className="d-grid gap-2">
+          {buttons}
+        </div>
       </div>
-    </div>
+    </>
   }
 }
