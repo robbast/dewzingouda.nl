977eaf631c3d194a8429dd44bb40891522515c74
Rob Bast <rob.bast@gmail.com>
2022-04-18 08:35:14 +0200

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,16 @@
 import { createRoot, Root } from 'react-dom/client'
 import App from './app'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    environment: 'production',
+    dsn: 'https://978729166dcf42ad854d9cfa323c4373@o172628.ingest.sentry.io/6341954',
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1,
+  })
+}
 
 const container: HTMLElement = document.getElementById('root') as HTMLElement,
   root: Root = createRoot(container)
